c18e24504bf39480ab19ff194f2ea94ed5101806
Luigi Selmi <luigiselmi@gmail.com>
2017-09-20 17:10:16 +0200

    changed path to resource data file

--- src/main/java/de/fraunhofer/cortex/recommender/RecommenderServlet.java
+++ src/main/java/de/fraunhofer/cortex/recommender/RecommenderServlet.java
@@ -67,7 +67,7 @@ public final class RecommenderServlet extends HttpServlet {
   @Override
   public void init(ServletConfig config) throws ServletException {
     super.init(config);
-    String recommenderClassName = "org.apache.mahout.cf.taste.example.grouplens.GroupLensRecommender";
+    String recommenderClassName = "de.fraunhofer.cortex.recommender.grouplens.GroupLensRecommender";
     //String recommenderClassName = config.getInitParameter("recommender-class");
     if (recommenderClassName == null) {
       throw new ServletException("Servlet init-param \"recommender-class\" is not defined");

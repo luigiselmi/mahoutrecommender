4b34a5e9c737b57d5cad6c1775f14b6ac2a688a7
Luigi Selmi <luigiselmi@gmail.com>
2018-05-11 16:21:59 +0200

    added min reload time interval configuration

--- src/main/java/de/fraunhofer/cortex/recommender/cf/ApplicationConfig.java
+++ src/main/java/de/fraunhofer/cortex/recommender/cf/ApplicationConfig.java
@@ -3,11 +3,21 @@ package de.fraunhofer.cortex.recommender.cf;
 public class ApplicationConfig {
   
   private String signalsFile;
+  private String minReloadIntervalMillis;
   
+  
+  public String getMinReloadIntervalMillis() {
+	return minReloadIntervalMillis;
+  }
+  
+  public void setMinReloadIntervalMillis(String minReloadIntervalMillis) {
+	this.minReloadIntervalMillis = minReloadIntervalMillis;
+  }
   
   public String getSignalsFile() {
     return signalsFile;
   }
+  
   public void setSignalsFile(String signalsFile) {
     this.signalsFile = signalsFile;
   }
